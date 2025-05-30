6d5fa25a74a592e77f40dc66487104100092b2c7
Hernan-99 <hernan24744@gmail.com>
2025-05-30 15:14:31 -0300

    layout por defecto de la app

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,15 +1,16 @@
 <template>
-  <header>
-    <h1>Header</h1>
-  </header>
-  <main>
+  <Header :brand="brand" :textBtn="textBtn" />
+  <main class="container mx-auto px-4 md:px-6">
     <slot />
   </main>
-  <footer>
-    <h3>Footer</h3>
-  </footer>
+  <Footer :copy="copy" />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Header from "../components/shared/Header.vue";
+import Footer from "../components/shared/Footer.vue";
 
-<style scoped></style>
+const brand: string = "Wabox";
+const textBtn: string = "Create new template";
+const copy: string = "© 2025 wabox - challenge";
+</script>
