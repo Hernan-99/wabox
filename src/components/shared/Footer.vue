d0a70148d11382db1fff63da2d53c1b6751f76fa
Hernan-99 <hernan24744@gmail.com>
2025-05-30 15:12:43 -0300

    componentes header y footer, compartidos en todas las vistas

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="bg-blue-500 py-8">
+    <p class="text-center text-xl text-white">{{ copy }}</p>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  copy: string;
+}
+
+const { copy } = defineProps<Props>();
+</script>
---
+++ src/components/shared/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="bg-blue-500 mb-20">
+    <nav class="mx-auto flex justify-between items-center px-12 py-8">
+      <div class="logo">
+        <RouterLink
+          to="/"
+          class="text-2xl font-sans text-white font-bold flex items-center gap-4"
+        >
+          <ChatBubbleBottomCenterTextIcon
+            class="text-sm text-white w-16 h-16 hover:-translate-y-1 ease-in-out duration-300"
+          />
+          {{ brand }}
+        </RouterLink>
+      </div>
+      <div>
+        <RouterLink
+          to="/create-template"
+          class="flex items-center justify-center px-4 bg-blue-700 font-bold text-white hover:text-blue-800 w-48 py-3 rounded-md hover:bg-white hover:translate-x-1 ease-in-out duration-300"
+        >
+          {{ textBtn }}
+        </RouterLink>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import { ChatBubbleBottomCenterTextIcon } from "@heroicons/vue/24/solid";
+import { RouterLink } from "vue-router";
+
+interface Props {
+  brand: string;
+  textBtn: string;
+}
+
+const { brand, textBtn } = defineProps<Props>();
+</script>
