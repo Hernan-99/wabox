6e1070fb2e9da18ac6ae6afa12ad5063c307141f
Hernan-99 <hernan24744@gmail.com>
2025-05-30 06:03:45 -0300

    estructura principal del layout

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <h3>Footer</h3>
+  </footer>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
