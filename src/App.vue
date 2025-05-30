8077e9e606a57f1047baf8174ecac71da3b1838c
Hernan-99 <hernan24744@gmail.com>
2025-05-30 05:45:13 -0300

    initial config

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  port: number;
+}
+
+const { port = 3000 } = defineProps<Props>();
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">Frontend is running in port {{ port }}!</h1>
+</template>
+
+<style scoped></style>
